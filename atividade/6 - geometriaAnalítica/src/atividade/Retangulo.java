f1ff054d0919eaa3666f197055bc340c3df94b36
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-03-18 17:20:40 -0300

    Criação dos construtores da class Retangulo.

--- atividade/6 - geometriaAnalítica/src/atividade/Retangulo.java
+++ atividade/6 - geometriaAnalítica/src/atividade/Retangulo.java
@@ -8,5 +8,11 @@ public class Retangulo {
 		this.point1 = point1;
 		this.point2 = point2;
 	}
+	public Retangulo(int coordX1, int coordY1, int coordX2, int coordY2) {
+		this.point1.setCoordinateX(coordX1);
+		this.point1.setCoordinateY(coordY1);
+		this.point2.setCoordinateX(coordX2);
+		this.point2.setCoordinateY(coordY2);
+	}
 	
 }
