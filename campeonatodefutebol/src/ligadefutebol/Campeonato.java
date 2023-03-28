629a09b55d19c85865614bc0efb63cfaf0de7d12
Felipe Ferraz <115377471+FelipeFerraz4@users.noreply.github.com>
2023-03-27 22:10:58 -0300

    Update Campeonato.java
    Construção da base dos métodos da classe campeonato

--- campeonatodefutebol/src/ligadefutebol/Campeonato.java
+++ campeonatodefutebol/src/ligadefutebol/Campeonato.java
@@ -13,9 +13,28 @@ public class Campeonato {
 	}
 	
 	public Clube[] getTimes() {
-		return times;
+		return this.times;
 	}
 	
-	
+	public void jogarCampeonato() {
+		
+		//fazer os encontro dos times e chamar 
+		//a função jogarPartida para cada
+		
+	}
+	public void jogarPartida() {
+		
+		//sortiar o placa e por os ponto nos clubes 
+		
+	}
+	public Clube[] getClassificacao(){
+		
+		//Ordenação de vetor de times
+		
+		return this.times;
+	}
+	public Clube getCampeao() {
+		return this.times[0];
+	}
 	
 }
